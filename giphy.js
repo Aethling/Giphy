4acb0828b937a19194725b2801adef60cd1816f1
Aric <aessig32@hotmail.com>
2018-02-26 14:31:30 -0600

    renders well but not perfectly. Added two click handlers

--- giphy.js
+++ giphy.js
@@ -1,22 +1,42 @@
 $(document).ready(function(){
-	// var url = "https://api.giphy.com/v1/gifs/random?api_key=muuUmVthfqiLAze1NqapDnjKyXv0ehgI&tag=&rating=G"
-	// $.getJSON(url, function(xhr){
-	// 		console.log(xhr.data);
-	// 		$firstGif = $("<img></img>").attr("src", xhr.data.images.fixed_height_downsampled.url);
-	// 	$(".row").append($firstGif);
-	// });
-	var url = "https://api.giphy.com/v1/gifs/trending?api_key=muuUmVthfqiLAze1NqapDnjKyXv0ehgI&tag=&rating=R"
-	$.getJSON(url, function(xhr){
-			console.log(xhr.data);
-			var gifHTML;
-			$.each(xhr.data, function(i, item){
-				if (i < 3) {				
-					gifHTML += '<img src="'+ item.images.fixed_width_downsampled.url + '">';
-				}
-				// } else if (i < 9) {
-
-				// }
-			})
-			$(".column").append(gifHTML);
+	$("button.random").on("click", function(){
+		randomGif();
 	});
+	$("button.trending").on("click", function(){
+		trendingGifs();
+	})
+	
+	function randomGif() {
+		var url = "https://api.giphy.com/v1/gifs/random?api_key=muuUmVthfqiLAze1NqapDnjKyXv0ehgI&tag=&rating=G"
+		$.getJSON(url, function(xhr){
+				$firstGif = $("<img></img>").attr("src", xhr.data.images.fixed_height_downsampled.url);
+			$(".column1").append($firstGif);
+		});
+	}
+	function trendingGifs() {
+		var url = "https://api.giphy.com/v1/gifs/trending?api_key=muuUmVthfqiLAze1NqapDnjKyXv0ehgI&tag=dance&rating=R"
+		$.getJSON(url, function(xhr){
+				var gifHTML;
+				$.each(xhr.data, function(i, item){
+					var html = '<img src="'+ item.images.fixed_width_downsampled.url + '">'
+					if (i < 7) {				
+						gifHTML += html;
+						$(".column1").append(gifHTML);
+						gifHTML = '';
+					} else if (i >= 7 && i <= 12 ) {
+						gifHTML += html;
+						$(".column2").append(gifHTML);
+						gifHTML = '';
+					} else if (i >= 13 && i <= 18) {
+						gifHTML += html;
+						$(".column3").append(gifHTML);
+						gifHTML = '';
+					} else if (i >=19 && i <= 25){
+						gifHTML += html;
+						$(".column4").append(gifHTML);
+						gifHTML = '';
+					}
+				})
+		});
+	}
 })

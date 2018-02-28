e624f3830dce729dfd3f5c860b459225c2993b29
Aric <aessig32@hotmail.com>
2018-02-28 07:38:42 -0600

    added function, changed backround color

--- giphy.js
+++ giphy.js
@@ -22,6 +22,10 @@ $(document).ready(function(){
 			limit: 25,
 			}
 		$.getJSON(theurl, data, function(xhr){
+			displayContent(xhr);
+		});
+	}
+		function displayContent(xhr) {
 			var gifHTML;
 			$.each(xhr.data, function(i, item){
 				var html = '<img src="'+ item.images.fixed_width_downsampled.url + '">'
@@ -43,9 +47,7 @@ $(document).ready(function(){
 					gifHTML = '';
 				}
 			});
-		})
-	}
-
+		}
 	function randomGif() {
 		var url = "https://api.giphy.com/v1/gifs/random?api_key=muuUmVthfqiLAze1NqapDnjKyXv0ehgI&tag=&rating=G"
 		$.getJSON(url, function(xhr){
@@ -56,27 +58,7 @@ $(document).ready(function(){
 	function trendingGifs() {
 		var url = "https://api.giphy.com/v1/gifs/trending?api_key=muuUmVthfqiLAze1NqapDnjKyXv0ehgI&q=&rating=R"
 		$.getJSON(url, function(xhr){
-				var gifHTML;
-				$.each(xhr.data, function(i, item){
-					var html = '<img src="'+ item.images.fixed_width_downsampled.url + '">'
-					if (i < 7) {				
-						gifHTML += html;
-						$(".column1").append(gifHTML);
-						gifHTML = '';
-					} else if (i >= 7 && i <= 12 ) {
-						gifHTML += html;
-						$(".column2").append(gifHTML);
-						gifHTML = '';
-					} else if (i >= 13 && i <= 18) {
-						gifHTML += html;
-						$(".column3").append(gifHTML);
-						gifHTML = '';
-					} else if (i >=19 && i <= 25){
-						gifHTML += html;
-						$(".column4").append(gifHTML);
-						gifHTML = '';
-					}
-				})
+			displayContent(xhr);
 		});
-	}
+	}	
 })
